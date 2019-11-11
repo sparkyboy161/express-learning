74c8aee972198f5df5f90ceb10c5c9e86d136797
sparkyboy161 <46911331+sparkyboy161@users.noreply.github.com>
2019-11-11 14:26:05 +0700

    user validation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -25,7 +25,26 @@ module.exports.create = function(req,res){
 };
 
 module.exports.postCreate = function(req,res){
-  req.body.id = shortid.generate();
+	var errors = [];
+	var values = [];
+	req.body.id = shortid.generate();
+
+	if(!req.body.name){
+		errors.push('Name is required');
+	}
+
+	if(!req.body.phone){
+		errors.push('Phone is required');
+	}
+
+	if(errors.length){
+		res.render("create.pug",{
+			errors: errors,
+			values: req.body
+		});
+		return;
+	}
+
   db.get('users').push(req.body).write();
   res.redirect('/users');
 }
