a4c2de12c0d9b7f050d9514470430a85cb50b807
sparkyboy161 <46911331+sparkyboy161@users.noreply.github.com>
2019-11-11 17:08:26 +0700

    try out cookie

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ var express = require('express');
 
 var userRoute = require('./routes/user.route');
 
+var express = require('express');
+var cookieParser = require('cookie-parser');
+
 var app = express();
 
 var port = 3000;
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(cookieParser());
+
 app.set("view engine", "pug");
 
 app.get('/',function(req,res){
--- routes/user.route.js
+++ routes/user.route.js
@@ -6,6 +6,11 @@ var validate = require('../validate/user.validate');
 
 	router.get('/', controller.index);
 
+	router.get('/test-cookie', function(req,res,next){
+		res.cookie('user-id',12345);
+		res.send('Sucessful');
+	});
+
 	router.get('/search', controller.search);
 
 	router.get('/create',controller.create);
@@ -13,5 +18,7 @@ var validate = require('../validate/user.validate');
 	router.post('/create', validate.validate , controller.postCreate);
 
 	router.get('/:id', controller.view);
+
+
 
 module.exports = router;
