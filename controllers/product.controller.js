45b9228394597fa5b0882716b5946ed58e95454b
sparkyboy161 <46911331+sparkyboy161@users.noreply.github.com>
2019-11-12 22:34:23 +0700

    product pagination

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,3 +1,14 @@
-module.exports.index = function(req,res){
+var db = require('../db');
 
+var products = db.get('products').value();
+
+module.exports.index = function(req,res){
+  var page = parseInt(req.query.page) || 1;
+  var perPage = 8;
+  var begin = (page-1)*perPage;
+  var end = page*perPage;
+
+  res.render('product/index.pug',{
+    products: products.slice(begin,end)
+  });
 };
