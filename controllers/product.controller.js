02d3b0d81119df946a514a3ae7f438bce876f95b
sparkyboy161 <46911331+sparkyboy161@users.noreply.github.com>
2019-11-17 09:13:26 +0700

    using mongodb for database

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,14 +1,18 @@
-var db = require('../db');
-
-var products = db.get('products').value();
+var Product = require('../models/product.model');
 
 module.exports.index = function(req,res){
-  var page = parseInt(req.query.page) || 1;
-  var perPage = 8;
-  var begin = (page-1)*perPage;
-  var end = page*perPage;
+  // var page = parseInt(req.query.page) || 1;
+  // var perPage = 8;
+  // var begin = (page-1)*perPage;
+  // var end = page*perPage;
+  //
+  // res.render('product/index.pug',{
+  //   products: products.slice(begin,end)
+  // });
 
-  res.render('product/index.pug',{
-    products: products.slice(begin,end)
+  Product.find().then(function(products){
+    res.render('product/index.pug',{
+      products: products
+    });
   });
 };
